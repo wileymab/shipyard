a63ce2a52850c7e3bfe4c0c2b0e861a8d6066a3d
Matt Wiley <wileymab@gmail.com>
2020-04-30 21:02:38 -0400

    Nuking to start a more simplified setup.

--- scripts/assemble.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-scriptDir=$(dirname ${BASH_SOURCE[0]})
-
-"${scriptDir}/render.sh" "${1}"
-"${scriptDir}/build.sh" "${1}"
--- src/python3-dev/build.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-scriptDir=$(dirname ${BASH_SOURCE[0]})
-source ${scriptDir}/../../.local/values.sh
-
-docker build \
-    -t python3-dev \
-    -f ${scriptDir}/../../${1} \
-    .
